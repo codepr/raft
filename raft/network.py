bb2698fa7de540f6084eacc4e09e427bf85bb72e
Andrea <a.g.baldan@gmail.com>
2019-10-20 23:51:14 +0200

    Moved out clients transport creation

--- raft/network.py
+++ raft/network.py
@@ -6,46 +6,35 @@ class RaftServerProtocol(asyncio.Protocol):
     def __init__(self, timeout, nodes=()):
         self.timeout = timeout
         self.nodes = nodes
-        loop = asyncio.get_running_loop()
-        self.heartbeat = loop.create_task(self.send_heartbeat())
-        self.client = loop.create_task(self.get_client(nodes))
+        self.loop = asyncio.get_running_loop()
+        self.nodes = nodes
 
     def connection_made(self, transport):
         peername = transport.get_extra_info('peername')
         print('Connection from {}'.format(peername))
         self.transport = transport
+        self.send_heartbeat()
 
     def datagram_received(self, data, addr):
         message = data.decode()
         print('Data received from {}: {!r}'.format(addr, message))
 
-    def send_data(self, data):
-        self.transport.sendto(data)
-
-    def close(self):
-        self.transport.close()
-
-    async def get_client(self, addr=('127.0.0.1', 20001)):
-        loop = asyncio.get_running_loop()
-        transport, _ = await loop.create_datagram_endpoint(
-            lambda: RaftClientProtocol(),
-            remote_addr=addr,
-            reuse_address=True
-        )
-        return transport
-
-    async def send_heartbeat(self):
-        client = await self.client
-        while True:
-            # TODO send heartbeat
-            client.sendto(b"BEAT")
-            await asyncio.sleep(self.timeout)
+    def send_heartbeat(self):
+        # TODO send heartbeat
+        for node in self.nodes:
+            node.sendto(b"BEAT")
+        self.loop.call_later(self.timeout, self.send_heartbeat)
 
 
-async def run_server(addr=('127.0.0.1', 20000), nodes=(), timeout=1):
+async def run_server(addr=('127.0.0.1', 20000), nodes_addrs=(), timeout=5):
     # Get a reference to the event loop as we plan to use
     # low-level APIs.
     loop = asyncio.get_running_loop()
+
+    nodes = await asyncio.gather(
+        *[get_client(node_addr) for node_addr in nodes_addrs],
+        loop=loop
+    )
 
     transport, protocol = await loop.create_datagram_endpoint(
         lambda: RaftServerProtocol(timeout, nodes),
@@ -67,3 +56,13 @@ class RaftClientProtocol(asyncio.Protocol):
 
     def error_received(self, exc):
         print('Received an error')
+
+
+async def get_client(self, addr=('127.0.0.1', 20001)):
+    loop = asyncio.get_running_loop()
+    transport, _ = await loop.create_datagram_endpoint(
+        lambda: RaftClientProtocol(),
+        remote_addr=addr,
+        reuse_address=True
+    )
+    return transport
